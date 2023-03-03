b7b4180c244a23abdf61a69c800292d3dd4f7a93
marianagonzales <placenciadulce@gmail.com>
2023-03-03 11:18:01 -0500

    algodondeazucar

--- unidad 4/firebase/crud/app/firebase.js
+++ unidad 4/firebase/crud/app/firebase.js
@@ -1,25 +1,41 @@
-export function hola (){
-    console.log('hola este mensaje es del main js');
+export function hola() {
+  console.log('hola este mensaje es del main js');
 }
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-analytics.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyDFAbGr20mZIHEYy3wkpPHVD2otj508WbE",
-    authDomain: "dulchi-53aaa.firebaseapp.com",
-    projectId: "dulchi-53aaa",
-    storageBucket: "dulchi-53aaa.appspot.com",
-    messagingSenderId: "1088047325836",
-    appId: "1:1088047325836:web:9cab520277271badb1d1a9",
-    measurementId: "G-VZ3Z86YYNP"
-  };
+import {
+  getFirestore, // conexion a bases de datos
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDFAbGr20mZIHEYy3wkpPHVD2otj508WbE",
+  authDomain: "dulchi-53aaa.firebaseapp.com",
+  projectId: "dulchi-53aaa",
+  storageBucket: "dulchi-53aaa.appspot.com",
+  messagingSenderId: "1088047325836",
+  appId: "1:1088047325836:web:9cab520277271badb1d1a9",
+  measurementId: "G-VZ3Z86YYNP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+//iniciar getFirestore
+const db = getFirestore();
+
+export const guardarTarea = (titulo, descrip) => {
+  addDoc(collection(db, "tareas"), {
+    _title: titulo,
+    _descripcion: descrip
+  });
+  console.log()
+}
--- unidad 4/firebase/crud/app/main.js
+++ unidad 4/firebase/crud/app/main.js
@@ -1,2 +1,19 @@
-import{hola} from "./firebase.js"
-hola();
+// import{hola} from "./firebase.js"
+// hola();
+import { guardarTarea } from "./firebase.js";
+window.addEventListener('DOMContentLoaded', () => {
+
+})
+
+let form = document.getElementById('formulario');
+
+form.addEventListener('submit', () => {
+    let titulo = document.getElementById('titulo').value;
+    let descrip = document.getElementById('descripcion').value;
+
+    // console.log(titulo,descripcion);
+    
+    // alert(titulo,descrip);
+
+    guardarTarea(titulo,descrip);
+})
